2ac5472d61ffdf11cd92748e7c6f12cd932e2c0e
Douglas Francis <dougiefrancis@gmail.com>
2021-02-28 08:46:08 +0000

    thank you and error pages added

--- contact.php
+++ contact.php
@@ -15,8 +15,8 @@ $mail->SMTPAuth   = TRUE;
 $mail->SMTPSecure = "tls";
 $mail->Port       = 587;
 $mail->Host       = "tls://smtp.gmail.com";
-$mail->Username   = "cheshiretownandcountry@gmail.com";
-$mail->Password   = "Clemjackie19222007";
+$mail->Username   = "dougiefrancis@gmail.com";
+$mail->Password   = "C2h6o123";
 
 $name = $_POST["name"];
 $email = $_POST["email"];
@@ -24,7 +24,7 @@ $number = $_POST["number"];
 $message = $_POST["message"];
 
 $mail->IsHTML(true);
-$mail->AddAddress("cheshiretownandcountry@gmail.com", "Richard");
+$mail->AddAddress("dougiefrancis@gmail.com", "Doug");
 $mail->SetFrom($email, $name);
 $mail->AddReplyTo($email, $name);
 $mail->Subject = "New Job Enquiry";
@@ -32,10 +32,11 @@ $content = "The job description is: $message <br> Contact them on: $number or em
 
 $mail->MsgHTML($content); 
 if(!$mail->Send()) {
-  echo "Error while sending Email.";
+    echo '<meta http-equiv="refresh" content="0;url=error.html" />';
   var_dump($mail);
 } else {
-  echo "Email sent successfully";
+    echo '<meta http-equiv="refresh" content="0;url=thanks.html" />';
+    exit;
 }
 
 
